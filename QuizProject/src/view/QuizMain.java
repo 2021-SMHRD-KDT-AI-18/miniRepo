88697f76021f087b7a04ea5f81cf85b42d811afd
smhrd <smhrd@210.183.87.94>
2024-01-30 10:43:41 +0900

    폰트설정 후 커밋

--- QuizProject/src/view/QuizMain.java
+++ QuizProject/src/view/QuizMain.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -16,9 +17,10 @@ public class QuizMain {
 		int rankH = 0;
 		int scoreL = 0;
 		
+		Font impactFont = new Font("Impact", Font.PLAIN, 14);
 		
 		String text = "본 프로그램은 정보처리기사시험 학습을 목표로 하여 'BGM'은 일부러 생략하였습니다."
-				+ "    명령어와 관련된 문제는 소문자로 작성하시고, 이외의 정답은  한글 및 영어 대문자로 작성하셔야 정상 정답처리됩니다. 또한, 정답을 입력할 때 띄어쓰기 없이 입력해 주세요. "
+				+ "    명령어와 관련된 문제는 소문자로 작성하시고, 이외의 정답은  한글 및 영어 대문자로 작성하셔야 정상 정답처리됩니다.  또한,  정답을 입력할 때 띄어쓰기 없이 입력해 주세요. "
 				;
 		
 		
@@ -41,16 +43,16 @@ public class QuizMain {
 		while (true) {
 			System.out.println("                                 ★정처기 꽉 붙 JAVA★");
 			System.out.println("=================메뉴==================");
-			System.out.print("1.회원가입 2.로그인 3.최고점수확인 4.종료 >>> ");
+			System.out.print("1.회원가입 2.로그인 3.최고점수확인 4.종료  >>>  ");
 			int a = sc.nextInt(); // 메뉴선택입력
 
 			if (a == 1) { // 회원가입
 				System.out.println("================회원등록================");
-				System.out.print("ID를 입력하세요 : ");
+				System.out.print("ID를 입력하세요  :  ");
 				String id = sc.next();
-				System.out.print("PW를 입력하세요 : ");
+				System.out.print("PW를 입력하세요  :  ");
 				String pw = sc.next();
-				System.out.print("닉네임을 입력하세요 : ");
+				System.out.print("닉네임을 입력하세요  :  ");
 				String nic = sc.next();
 				System.out.println();
 				
@@ -60,9 +62,9 @@ public class QuizMain {
 				System.out.println();
 			} else if (a == 2) { // 로그인
 				System.out.println("================로그인=================");
-				System.out.print("ID를 입력하세요 : ");
+				System.out.print("ID를 입력하세요  :  ");
 				String id = sc.next();
-				System.out.print("PW를 입력하세요 : ");
+				System.out.print("PW를 입력하세요  :  ");
 				String pw = sc.next();
 				DTO info = controller.login(id, pw);
 				System.out.println();
@@ -70,7 +72,7 @@ public class QuizMain {
 					System.out.println("★ "+info.getNic() + "님 안녕하세요! ★");
 					System.out.println();
 					System.out.println("==============난이도 선택===============");
-					System.out.print("1.이지 2.하드 >> ");
+					System.out.print("1.이지 2.하드  >>  ");
 					int b = sc.nextInt();
 					System.out.println();
 					if (b == 1) { // 이지모드 게임실행
@@ -87,7 +89,7 @@ public class QuizMain {
 							int num = ran.nextInt(list.size());
 							String l_ans = list.get(num).getAnswer().substring(0, 1);
 							System.out.println(list.get(num).getQuestion());
-							System.out.print("Q" + cnt + ". 정답 입력 : " + l_ans);
+							System.out.print("Q" + cnt + ". 정답 입력  :  " + l_ans);
 							String ans = sc.next();
 							if (list.get(num).getAnswer().equals(l_ans + ans)) {
 								System.out.println("◆ 정답입니다. ◆");
@@ -113,7 +115,7 @@ public class QuizMain {
 							System.out.println("▶ 오답풀이를 시작합니다. ◀");
 							for (int i = 0; i < wlist.size(); i++) {
 								System.out.println(wlist.get(i).getQuestion());
-								System.out.print("Q" + (i + 1) + ". 정답 입력 : ");
+								System.out.print("Q" + (i + 1) + ". 정답 입력  :  ");
 								String ans = sc.next();
 								if (wlist.equals(ans)) {
 									System.out.println("◆ 정답입니다. ◆");
@@ -143,7 +145,7 @@ public class QuizMain {
 
 							int num = ran.nextInt(list.size());
 							System.out.println(list.get(num).getQuestion());
-							System.out.print("Q" + cnt + ". 정답 입력 : ");
+							System.out.print("Q" + cnt + ". 정답 입력  :  ");
 							String ans = sc.next();
 							if (list.get(num).getAnswer().equals(ans)) {
 								System.out.println("◆ 정답입니다. ◆");
@@ -163,14 +165,14 @@ public class QuizMain {
 						System.out.printf("총 %d문제 중 %d문제를 맞추셨습니다.%n", cnt - 1, correct);
 						System.out.println();
 						System.out.println("틀린문제를 다시 푸시겠습니까?");
-						System.out.print("1.예 2.종료 >> ");
+						System.out.print("1.예 2.종료 >>  ");
 						int c = sc.nextInt();
 						System.out.println();
 						if (c == 1) {
 							System.out.println("▶ 오답풀이를 시작합니다. ◀");
 							for (int i = 0; i < wlist.size(); i++) {
 								System.out.println(wlist.get(i).getQuestion());
-								System.out.print("Q" + (i + 1) + ". 정답 입력 : ");
+								System.out.print("Q" + (i + 1) + ". 정답 입력  :  ");
 								String ans = sc.next();
 								if (wlist.equals(ans)) {
 									System.out.println("◆ 정답입니다. ◆");
@@ -193,7 +195,7 @@ public class QuizMain {
 				}
 			} else if (a == 3) {
 				
-				System.out.print("ID를 입력해주세요 >> ");
+				System.out.print("ID를 입력해주세요 >>  ");
 				String id = sc.next();
 				System.out.println("==============최고점수확인===============");
 
